28a93995d7b7889380f0a07c786b95446e4cc6ce
SzaboKolos <kolor1400@gmail.com>
2022-05-02 19:19:58 +0200

    Done

--- src/webshop/Payment.java
+++ src/webshop/Payment.java
@@ -1,5 +1,78 @@
 package webshop;
 
-public class Payment {
+import java.time.LocalDate;
 
+public class Payment {
+    private String webshopId;
+    private String customerId;
+    private PaymentMethod paymentMethod;
+    private Integer price;
+    private Long accountNumber;
+    private Long cardNumber;
+    private LocalDate paymentDate;
+
+    public Payment(){}
+    public Payment(String webshopId, String customerId, PaymentMethod paymentMethod, Integer price, Long accountNumber, Long cardNumber, LocalDate paymentDate) {
+        this.webshopId = webshopId;
+        this.customerId = customerId;
+        this.paymentMethod = paymentMethod;
+        this.price = price;
+        this.accountNumber = accountNumber;
+        this.cardNumber = cardNumber;
+        this.paymentDate = paymentDate;
+    }
+
+    public String getWebshopId() {
+        return webshopId;
+    }
+    public void setWebshopId(String webshopId) {
+        this.webshopId = webshopId;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Long getAccountNumber() {
+        return accountNumber;
+    }
+    public void setAccountNumber(Long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public Long getCardNumber() {
+        return cardNumber;
+    }
+    public void setCardNumber(Long cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public LocalDate getPaymentDate() {
+        return paymentDate;
+    }
+    public void setPaymentDate(LocalDate paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    @Override
+    public String toString() {
+        return (webshopId + ";" + customerId + ";" + paymentMethod + ";" + price + ";" + accountNumber + ";" + cardNumber + ";" + paymentDate);
+    }
 }
